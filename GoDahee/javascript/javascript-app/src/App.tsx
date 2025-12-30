a40990f98114c7afa69e94d0caec2993bc63b877
ebsibar18-crypto <ebsibar18@gmail.com>
2025-12-30 13:38:50 +0900

    feat: [고다희] Math 기능 및 switch 키워드 활용 [PMIF-x]

--- GoDahee/javascript/javascript-app/src/App.tsx
+++ GoDahee/javascript/javascript-app/src/App.tsx
@@ -4,6 +4,7 @@ import { CalculationExample } from './math_operation/CalculationExample'
 import { LogicalOperationExample } from './logical_operation/LogicalOperationExample'
 import { ControlFlowIf } from './control_flow/ControlFlowIf'
 import { FristProblem } from './assets/problem/FristProblem'
+import { ControlFlowSwitch } from './control_flow/ControlFlowSwitch'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
         <LogicalOperationExample/>
         <ControlFlowIf/>
         <FristProblem/>
+        <ControlFlowSwitch/>
       </div>
     </>
   )
